7866082ab6feaad6b208941520837a97e136142e
RaviTejaPulibanti <ravitejapulibanti18@gmail.com>
2026-08-31 23:30:46 +0530

    updated

--- HEC-Backend/src/server.ts
+++ HEC-Backend/src/server.ts
@@ -15,7 +15,7 @@ import { startExamStatusUpdater } from "./utils/cron.js";
 const app = express();
 
 app.use(cors()); 
-app.use(express.json()); 
+app.use(express.json({ limit: "50mb" }));
 
 
 app.use("/api/auth", authRoutes);
